b5aca1a701d861aa2e2d760d49506e64e3fc8d96
shari-singh <shari.singh93@gmail.com>
2016-05-31 16:01:08 -0400

    added comments

--- exercise4.rb
+++ exercise4.rb
@@ -1,11 +1,11 @@
-(1..100).each do |a|
-  if (a % 3 == 0) && (a % 5 == 0) == true
+(1..100).each do |a|  #repeat the function 100 times
+  if (a % 3 == 0) && (a % 5 == 0) == true #check if multiples of 3 and 5
     puts "BitMaker"
-  elsif a % 3 ==0
+  elsif a % 3 ==0 #checks for multiples of 3
     puts "Bit"
-  elsif a % 5 == 0
+  elsif a % 5 == 0 #checks for multiples of 5
     puts "Maker"
   else
-    puts a
+    puts a #prints integer if not multiple of 3 or 5
   end
 end
